25028f752aecaef4385b5916d3c506e40e182492
Chris Pettitt <cpettitt@gmail.com>
2015-02-08 07:50:11 -0800

    Paint sprites ordered by y-coordinate

--- src/system/renderer.js
+++ src/system/renderer.js
@@ -6,17 +6,20 @@ export default function(root, w, h) {
   root.appendChild(renderer.view);
 
   return function(entities) {
-    entities.forEach(function(entity) {
-      if (entity.position && entity.sprite) {
-        if (!entity.sprite.pixi) {
-          var texture = PIXI.Texture.fromImage("img/" + entity.sprite.name + ".png");
-          var sprite = new PIXI.Sprite(texture);
-          stage.addChild(sprite);
-          entity.sprite.pixi = sprite;
-        }
+    stage.removeChildren();
 
-        entity.sprite.pixi.position = entity.position;
-      }
+    var visibles = entities.filter(function(entity) {
+      return entity.position && entity.sprite;
+    });
+    visibles.sort(function(l, r) {
+      return l.position.y - r.position.y;
+    });
+
+    visibles.forEach(function(entity) {
+      var texture = PIXI.Texture.fromImage("img/" + entity.sprite.name + ".png");
+      var sprite = new PIXI.Sprite(texture);
+      stage.addChild(sprite);
+      sprite.position = entity.position;
     });
 
     renderer.render(stage);
